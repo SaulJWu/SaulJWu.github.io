1070be390c7aa66572dbfb5785d3377e02faddba
SaulJWu <saul.j.wu@outlook.com>
2020-11-17 10:46:30 -0400

    手动 提交

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,55 @@
+#!/usr/bin/env sh
+# 确保脚本抛出遇到的错误
+set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
+# deploy to github
+# echo 'blog.xugaoyi.com' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  # git@github.com:SaulJWu/SaulJWu.github.io.git
+  githubUrl=git@github.com:SaulJWu/SaulJWu.github.io.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://SaulJWu:${GITHUB_TOKEN}@github.com/SaulJWu/SaulJWu.github.io.git
+  git config --global user.name "SaulJWu"
+  git config --global user.email "SaulJWu@outlook.com"
+fi
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
-git push -f https://github.com/SaulJWu/SaulJWu.github.io.git master
+# # deploy to coding
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
+# else
+#   codingUrl=https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
-cd - # 退回开始所在目录
+cd -
 rm -rf docs/.vuepress/dist
+
+
+# 旧版
+# =====================
+
+# # 生成静态文件
+# npm run build
+
+# # 进入生成的文件夹
+# cd docs/.vuepress/dist
+
+# git init
+# git add -A
+# git commit -m 'deploy'
+
+# git push -f https://github.com/SaulJWu/SaulJWu.github.io.git master
+
+# cd - # 退回开始所在目录
+# rm -rf docs/.vuepress/dist
