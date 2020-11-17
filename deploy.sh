9992564ce484744088aaf94ac0e4b04379ab51a2
SaulJWu <saul.j.wu@outlook.com>
2020-11-17 11:56:35 -0400

    new

--- deploy.sh
+++ deploy.sh
@@ -34,22 +34,3 @@ git push -f $githubUrl master:gh-pages # 推送到github
 
 cd -
 rm -rf docs/.vuepress/dist
-
-
-# 旧版
-# =====================
-
-# # 生成静态文件
-# npm run build
-
-# # 进入生成的文件夹
-# cd docs/.vuepress/dist
-
-# git init
-# git add -A
-# git commit -m 'deploy'
-
-# git push -f https://github.com/SaulJWu/SaulJWu.github.io.git master
-
-# cd - # 退回开始所在目录
-# rm -rf docs/.vuepress/dist
