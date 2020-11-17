e393ba223e97fee9cc9d655940bb5424bd376ce2
SaulJWu <saul.j.wu@outlook.com>
2020-11-17 14:32:20 -0400

    push

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,15 +1,16 @@
 module.exports = [
   { text: '首页', link: '/' },
-
   // {
-  //   text: '技术',
+  //   text: '后端',
   //   link: '/technology/',
-  //   items: [
-  //     { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-  //     { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-  //     { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-  //     { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-  //   ],
+  // },
+  // {
+  //   text: '前端',
+  //   link: '/web/',
+  // },
+  // {
+  //   text: '更多',
+  //   link: '/more/',
   // },
   { text: '分类', link: '/categories/' },
   { text: '标签', link: '/tags/' },
