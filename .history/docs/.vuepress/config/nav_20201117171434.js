d31a6dccc79663f02cb86108a5324f18e50d29c6
SaulJWu-PC <Saul.J.Wu@outlook.com>
2020-11-17 17:58:44 -0400

    push

--- .history/docs/.vuepress/config/nav_20201117171434.js
+++ .history/docs/.vuepress/config/nav_20201117171434.js
@@ -15,6 +15,5 @@ module.exports = [
   { text: '分类', link: '/categories/' },
   { text: '标签', link: '/tags/' },
   { text: '归档', link: '/archives/' },
-  { text: '友情链接', link: '/friends/' },
   // { text: '关于', link: '/about/' },
 ]
