dad111b6e05361f31fb687676c169c1f6e27b56c
SaulJWu <saul.j.wu@outlook.com>
2021-02-15 22:26:37 -0400

    Update nav.js

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,20 +1,21 @@
 module.exports = [
-  { text: '首页', link: '/' },
+  { text: "首页", link: "/" },
   {
-    text: '后端',
-    link: '/technology/',
+    text: "后端",
+    link: "/technology/",
+  },
+  { text: "分布式", link: "/distributed/" },
+  {
+    text: "前端",
+    link: "/web/",
   },
   {
-    text: '前端',
-    link: '/web/',
+    text: "更多",
+    link: "/more/",
   },
-  {
-    text: '更多',
-    link: '/more/',
-  },
-  { text: '分类', link: '/categories/' },
-  { text: '标签', link: '/tags/' },
-  { text: '归档', link: '/archives/' },
-  { text: '友情链接', link: '/friends/' },
-  { text: '关于', link: '/about/' },
-]
+  { text: "分类", link: "/categories/" },
+  { text: "标签", link: "/tags/" },
+  { text: "归档", link: "/archives/" },
+  { text: "友情链接", link: "/friends/" },
+  { text: "关于", link: "/about/" },
+];
