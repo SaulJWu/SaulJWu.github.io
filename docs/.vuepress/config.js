0273c72a0e9eded3133bbb839607e19d564138e2
SaulJWu <saul.j.wu@outlook.com>
2021-06-27 22:24:45 +0800

    自动提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -92,7 +92,7 @@ module.exports = {
     blogger: {
       // 博主信息，显示在首页侧边栏
       avatar:
-        "https://raw.githubusercontent.com/SaulJWu/images/main/20210208160252.jpg",
+        "https://gitee.com/SaulJWu/blog-images/raw/master/images/20210627222322.jpg",
       name: "Saul.J.Wu",
       slogan: "立身之本，不在高低。",
     },
